207ee739627fe33f7d72323b06f3e0757bcf1f83
Gunar Ortiz <gunarortiz@gmail.com>
2018-11-14 18:24:13 -0400

    count days

--- app/src/main/java/com/gunar/cumple/MainActivity.java
+++ app/src/main/java/com/gunar/cumple/MainActivity.java
@@ -2,12 +2,65 @@ package com.gunar.cumple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nombre, fecha;
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+    }
+
+
+    String days(String cumple){
+        try {
+            //Dates to compare
+            int year = Calendar.getInstance().get(Calendar.YEAR);
+
+            String CurrentDate=  "09/24/2015";
+            String FinalDate=  cumple.substring(0, 6)+year;
+
+            Date date1;
+            Date date2;
+
+            SimpleDateFormat dates = new SimpleDateFormat("MM/dd/yyyy");
+
+            //Setting dates
+            date1 = dates.parse(CurrentDate);
+            date2 = dates.parse(FinalDate);
+
+            //Comparing dates
+            long difference = Math.abs(date1.getTime() - date2.getTime());
+            long differenceDates = difference / (24 * 60 * 60 * 1000);
+
+            //Convert long to String
+            String dayDifference = Long.toString(differenceDates);
+
+            return dayDifference;
+        }
+        catch (Exception exception) {
+            Log.e("DIDN'T WORK", "exception " + exception);
+        }
+        return "";
     }
 }
